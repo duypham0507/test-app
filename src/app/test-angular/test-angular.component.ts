e74cb428f3eb7d5bc3d0ef1e75f8b4fab12bbf75
Pham Duy <duybka60@gmail.com>
2021-08-05 21:53:39 +0700

    go1

--- src/app/test-angular/test-angular.component.ts
+++ src/app/test-angular/test-angular.component.ts
@@ -9,22 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-angular.component.css'],
 })
 export class TestAngularComponent implements OnInit {
-  people: Person[] = [
-    {id: 1, name: 'Bố', age: 60, address: 'Thái Bình'},
-    {id: 1, name: 'Mẹ', age: 55, address: 'Thái Bình'},
-    {id: 1, name: 'Chị', age: 31, address: 'Hà Nội'},
-    {id: 1, name: 'Tôi', age: 23, address: 'Hà Nội'},
-  ];
-  displayedColumns: string[] = ['id', 'name', 'age', 'address'];
-  dataSource = this.people.map(a => {
-    return Object.assign([],
-      {
-        id: a.id,
-        name: a.name,
-        age: a.age
-      });
-    
-  });
+
 
   constructor() {
 
@@ -32,8 +17,8 @@ export class TestAngularComponent implements OnInit {
 
 
   ngOnInit(): void {
-    console.log(this.dataSource)
+
   }
 
-  
+
 }
